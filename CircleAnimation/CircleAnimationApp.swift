db923149042ea82aab481aba70938431d27b9505
Morten Just <morten.just@genstart.dk>
2023-01-14 22:00:02 +0100

    Initial Commit

---
+++ CircleAnimation/CircleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleAnimationApp.swift
+//  CircleAnimation
+//
+//  Created by Morten Just on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CircleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
